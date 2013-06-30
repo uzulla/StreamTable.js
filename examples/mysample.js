9a63ed6ea5e5a8317588b442d342b05988c37c43
uzulla <uzulla@himitsukichi.com>
2013-06-30 16:59:23 +0900

    add Item select Checkbox

--- examples/mysample.js
+++ examples/mysample.js
@@ -25,12 +25,14 @@ $(document).ready(function() {
     pagination_before: function(){
     },
     pagination: function(summary){
-      //save now check
+
       $('input[name^='+checkbox_prefix+']').each(function(){
         var elm = $(this);
         var idx = elm.attr('name').replace(checkbox_prefix_reg, '');
         if(checkbox_data[idx]){
-          elm.attr('checked', true);
+          elm.prop('checked', true);
+        }else{
+          elm.prop('checked', false);
         }
       });
 
@@ -39,7 +41,9 @@ $(document).ready(function() {
       }else{
         $found.hide();
       }
+
       $summary.text( summary.total + '件中、' + summary.from + '件目から'+ summary.to +'件目まで表示');
+
     },
   }
 
@@ -63,10 +67,45 @@ $(document).ready(function() {
     var idx = elm.attr('name').replace(checkbox_prefix_reg, '');
     if(elm.prop('checked')){
       checkbox_data[idx] = true;
+      $('#all_cb_control').prop('checked', true);
     }else{
-      checkbox_data[idx] = false;
+      delete checkbox_data[idx];
+
     }
     console.log(checkbox_data);
   }
+
+  $('#all_cb_control').click(function(){
+    if($('#all_cb_control').prop('checked')){ // 全部外す処理
+      if(confirm('すべてのチェックをつけますか？(現在のページ以外もチェックがされます)')){
+        //つける
+        var data_length = data.length;
+        for(var i=0; data_length>=i; i++){
+          checkbox_data[i] = true;
+        }
+        st.current_page = 0;
+        if(st.last_search_text.length == 0){
+          st.render(data, st.current_page);
+        }else{
+          st.render(st.last_search_result, st.current_page);
+        }
+        st.renderPagination(st.pageCount(), st.current_page);
+        st.execCallbacks('pagination');
+      }
+    }else{ // 全部つける処理
+      if(confirm('すべてのチェックを外しますか？(現在のページ以外もチェックが外されます)')){
+        //はずす
+        checkbox_data = {};
+        st.current_page = 0;
+        if(st.last_search_text.length == 0){
+          st.render(data, st.current_page);
+        }else{
+          st.render(st.last_search_result, st.current_page);
+        }
+        st.renderPagination(st.pageCount(), st.current_page);
+        st.execCallbacks('pagination');    
+      }
+    }
+  })
 
 });
